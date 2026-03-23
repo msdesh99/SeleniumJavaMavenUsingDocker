3c0abd7f129a75208eb19c4e20270750e6d30063
msdesh99 <131903278+msdesh99@users.noreply.github.com>
2026-03-22 20:30:22 -0400

    Added Logging using Log4j2 (#4)
    * Added one testCase and .gitignore
    
    * Added required changes mentioned in pull request
    
    * Added message for google test
    
    * Added Logging using Log4j2

--- testrunner.bat
+++ testrunner.bat
@@ -1,14 +1,18 @@
-one Way:
-//network created and docker compose up done
-docker compose up
-//run image as container
-docker run -it --network maventests-runoncontainer_seleniumgrid4 --name testrunnercontainer1 -e HOST_URL=http://HubService:4444/wd/hub -e OUTPUT_DIR=/app/output -v ${pwd}/output:/app/output testrunner
-2nd Way:
-//create container without running
-docker create -v testunnerVol:/app/output -v ${pwd}/output:/app/output --name testrunnerC1 maventests-runoncontainer-testrunner 
-docker network connect maventests-runoncontainer_seleniumgrid4 testrunnerC1  //connect with network
-docker start -ai testrunnerC1 //start and execute container
-docker run -it --network maventests-runoncontainer_seleniumgrid4 --name testrunnercontainer1 testrunner
+--Build image on docker :
+   1. docker build -t maven-tests .
+--compose environment and network 
+    2. docker compose up
+-To run maven tests in container
+    --one Way:
+        //network created and docker compose up done
+        //run image as container
+        3. docker run -it --network maventests-runoncontainer_seleniumgrid4 --name testrunnerMC1 
+        -e HOST_URL=http://HubService:4444/wd/hub -e OUTPUT_DIR=/app/output testrunner
+    --2nd OR
+        //create container without running
+        3. docker create -v ./output:/app/output --name testrunnerMC1 maventests-runoncontainer-testrunner 
+        4. docker network connect maventests-runoncontainer_seleniumgrid4 testrunnerMC1  //connect with network
+        5. docker start -ai testrunnerMC1 //start and execute container
 
 ---How to see the volumes:
 one way on windows poweshell
@@ -16,3 +20,5 @@ one way on windows poweshell
     $ls /app/output
 in docker desktop:
     go to the container> exec> cd /app/output    
+
+--normally bind mount should : \\wsl.localhost\docker-desktop\mnt\docker-desktop-disk\data\docker\containers
